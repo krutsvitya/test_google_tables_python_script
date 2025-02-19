f135cdfa65cd8dc44f5d908a9414a85351098879
krutsvitya <krutsvitya@gmail.com>
2025-02-19 21:02:32 +0100

    done

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import time
 from collections import defaultdict
 
 import gspread
@@ -12,39 +13,40 @@ client = gspread.authorize(creds)
 spreadsheet = client.open_by_key("15ai333vnsMIkvPaS90vsqBayQ7iL6XIvJyOi18wOrJY")
 
 worksheet = spreadsheet.sheet1
+while True:
+    students, teachers, lessons, teacher_availability = generate_random_data()
+    print(teachers)
+    lesson_groups = defaultdict(list)
 
-students, teachers, lessons, teacher_availability = generate_random_data()
-print(teachers)
-lesson_groups = defaultdict(list)
+    for lesson in lessons:
+        key = (lesson.teacher.name, lesson.time)
+        lesson_groups[key].append(lesson)
 
-for lesson in lessons:
-    key = (lesson.teacher.name, lesson.time)
-    lesson_groups[key].append(lesson)
+    headers = worksheet.row_values(1)
+    for key, group in lesson_groups.items():
+        if len(group) > 1:
+            print(f"Викладач {key[0]} проводить декілька зайнять у {key[1]}:")
 
-headers = worksheet.row_values(1)
-for key, group in lesson_groups.items():
-    if len(group) > 1:
-        print(f"Викладач {key[0]} проводить декілька зайнять у {key[1]}:")
+            schedule_conflict = {
+                "Teacher": key[0],
+                "Date": key[1].strftime('%Y-%m-%d'),
+            }
+            for teacher in teachers:
+                if teacher.name not in teacher_availability[key[1]]:
+                    schedule_conflict["New teacher"] = teacher.name
+                    teacher_availability[key[1]].append(teacher.name)
+                else:
+                    schedule_conflict["New teacher"] = 'Немає заміни'
 
-        schedule_conflict = {
-            "Teacher": key[0],
-            "Date": key[1].strftime('%Y-%m-%d'),
-        }
-        for teacher in teachers:
-            if teacher.name not in teacher_availability[key[1]]:
-                schedule_conflict["New teacher"] = teacher.name
-                teacher_availability[key[1]].append(teacher.name)
-            else:
-                schedule_conflict["New teacher"] = 'Немає заміни'
+            for i, lesson in enumerate(group):
+                schedule_conflict[f"Student{i+1}"] = lesson.student.name
+                print(f"  - {lesson}")
 
-        for i, lesson in enumerate(group):
-            schedule_conflict[f"Student{i+1}"] = lesson.student.name
-            print(f"  - {lesson}")
+            row_data = []
+            for header in headers:
+                row_data.append(schedule_conflict.get(header, ""))
 
-        row_data = []
-        for header in headers:
-            row_data.append(schedule_conflict.get(header, ""))
+            data = worksheet.append_row(row_data)
 
-        data = worksheet.append_row(row_data)
-
-print(data)
+    print(data)
+    time.sleep(18000)
